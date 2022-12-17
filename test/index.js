8547c60b1b5d158997ed2a2797344c684c40860c
Sam Morey <44871756+samm459@users.noreply.github.com>
2022-12-17 20:14:47 +0000

    Pending changes exported from your codespace

---
+++ test/index.js
@@ -0,0 +1,27 @@
+// global objects available by the platinum runtime
+// ------------------------------------------------
+// System
+// Process
+// File
+// Network
+// Module
+
+// System
+// ------------------------------------------------
+// System.platform -- The system's platform, e.g. "linux"
+// System.architecture -- The system's architecture, e.g. "x86_64"
+// System.name -- The system's name, e.g. 'codespaces-b55cb3'
+
+// Process
+// ------------------------------------------------
+// Process.pid -- The pid of the current process
+// Process.stdin -- An async iterator that yields a string each time the stdin on the process is written to
+
+// File
+// ------------------------------------------------
+// File.open -- Read the contents of a file
+// Files.constructor -- create a new file object
+
+// Network
+// -------------------------------------------------
+// Network.bind -- Bind
